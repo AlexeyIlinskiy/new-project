bbe2e4f4b4ced47cb56e921922e6fc03c22d6f4b
AlexeyIlinskiy <98812175+AlexeyIlinskiy@users.noreply.github.com>
2022-08-13 11:24:16 +0300

    Подключил api и добавил хуки в app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import styles from './app.module.css';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
-import data from '../../utils/data';
+
+const api = 'https://norma.nomoreparties.space/api/ingredients';
 
 function App() {
+  const [apiData, setApiData] = useState([]);
+  
+  useEffect(() => {
+    fetch(api)
+        .then(res => {
+          if (res.ok) {
+              return res.json();
+          }
+          return Promise.reject(`Ошибка ${res.status}`);
+        })
+        .then(data => setApiData(data.data))
+        .catch(e => {
+          console.log('Error: ' + e.message);
+        });
+  }, []);
+
   return (
     <div className="app">
       <AppHeader />
       <main className={styles.main}>
-        <BurgerIngredients ingredients={data}/>
-        <BurgerConstructor construct={data}/>
+        <BurgerIngredients ingredients={apiData}/>
+        <BurgerConstructor construct={apiData}/>
       </main>
     </div>
   );
