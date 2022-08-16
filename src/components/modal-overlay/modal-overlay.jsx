7d0f8c5bdb6d5e0a23430fb6ac474fa3029c5c75
AlexeyIlinskiy <98812175+AlexeyIlinskiy@users.noreply.github.com>
2022-08-16 12:41:22 +0300

    Пофиксил замечание по порталу

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import styles from './modal-overlay.module.css';
 
 import PropTypes from 'prop-types';
 
-import { modals } from '../../utils/constants';
-
-function ModalOverlay({ children, onClick }) {
-    return ReactDOM.createPortal(
-        (
-          <div className={styles.root} onClick={onClick}>
-              {children} 
-          </div>
-        ), 
-        modals
-    );
+function ModalOverlay({onClick }) {
+    
+  return (
+    <div className={styles.root} onClick={onClick}>
+    </div>
+  )
 };
 
 ModalOverlay.propTypes = {
-  children: PropTypes.node.isRequired,
   onClick: PropTypes.func.isRequired
 }
 
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import styles from './modal.module.css';
 
 import PropTypes from 'prop-types';
@@ -6,6 +7,7 @@ import PropTypes from 'prop-types';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import ModalOverlay from '../modal-overlay/modal-overlay';
+import { modals } from '../../utils/constants';
 
 function Modal ({header, children, onClose}) {
 
@@ -23,8 +25,10 @@ function Modal ({header, children, onClose}) {
   },[]);
   
 
-    return (
-      <ModalOverlay onClick={onClose}>
+    return ReactDOM.createPortal (
+      (
+      <>
+        <ModalOverlay onClick={ onClose } />  
         <div className={`${styles.root} pt-10 pr-10 pb-15 pl-10`}>
           <div className={styles.header}>
             { header && <h2 className="text_type_main-large">{header}</h2>}
@@ -34,7 +38,8 @@ function Modal ({header, children, onClose}) {
           </div>
             {children}
         </div>
-      </ModalOverlay>
+      </>
+      ), modals 
   );
 };
 
